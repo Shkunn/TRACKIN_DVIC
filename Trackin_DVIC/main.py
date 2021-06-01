5d0a9c1b2bdd2afeb3ebb3c95aa3f59b9816c55e
Shkunn <leclercqted@gmail.com>
2021-06-01 15:08:29 +0200

    [WIP]

--- Trackin_DVIC/main.py
+++ Trackin_DVIC/main.py
@@ -48,7 +48,7 @@ def initialize():
     args = parser.parse_args()
 
     # DEBUG OPTION.
-    if(args.debug == 1):
+    if(args.debug == '1'):
         is_debug_option = True
     
     # ZED CAMERA CONFIGURATION.        
@@ -320,8 +320,8 @@ def thread_compute_command(params):
         # os.system('cls' if os.name == 'nt' else 'clear')
         # print("Data Ultra song : ", data_ultrasensor)
         np.set_printoptions(suppress = True)
-        # if(is_debug_option):
-        print("Data position   : ", data_position)
+        if(is_debug_option):
+            print("Data position   : ", data_position)
         # print("Data detection  : ", data_detection)
         # print("Robot_state     : ", global_state)
         # print("User command    : ", user_command)
