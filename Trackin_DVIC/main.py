7699c2fb973351f22d5a2ee874178670379dfa20
thomasraynal98 <thomasraynal98@icloud.com>
2021-06-02 17:56:39 +0200

    [WIP]

--- Trackin_DVIC/main.py
+++ Trackin_DVIC/main.py
@@ -272,7 +272,7 @@ def thread_slam(params):
         if validation:
 
             humain        = objects.object_list[i].bounding_box_2d
-            # print(humain[0][0], humain[0][1])
+            print("POINT_A", humain[0][0], humain[0][1])
             point_A       = (int(humain[0][0]), int(humain[0][1]))
             point_B       = (int(humain[1][0]), int(humain[1][1]))
             point_C       = (int(humain[2][0]), int(humain[2][1]))
@@ -283,7 +283,7 @@ def thread_slam(params):
             image_draw    = cv2.line(image_draw, point_C, point_D, color, 5)  
             image_draw    = cv2.line(image_draw, point_D, point_A, color, 5) 
 
-            data_detection[0] = ((int(humain[0][0]) + int(humain[1][0]))/2) - (image_draw.shape[0]/2)
+            data_detection[0] = ((int(humain[0][0]) + int(humain[1][0]))/2) - (image_draw.shape[1]/2)
             data_detection[1] = objects.object_list[i].position[0] 
             data_detection[2] = len(objects.object_list)
         else:
