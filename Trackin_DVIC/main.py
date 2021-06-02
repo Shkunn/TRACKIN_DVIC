2062fa202f8d638727eaf31ef927c535faa8fb23
thomasraynal98 <thomasraynal98@icloud.com>
2021-06-02 18:53:27 +0200

    [WIP]

--- Trackin_DVIC/main.py
+++ Trackin_DVIC/main.py
@@ -332,8 +332,8 @@ def thread_compute_command(params):
         last_time = time.time()
         # ultra son data.
         # os.system('cls' if os.name == 'nt' else 'clear')
-        # print("Data Ultra song : ", data_ultrasensor)
         np.set_printoptions(suppress = True)
+        print("Data Ultra song : ", data_ultrasensor)
         print("Data position   : ", data_position)
         print("Data detection  : ", data_detection)
         print("Robot_state     : ", global_state)
@@ -371,27 +371,27 @@ def thread_compute_command(params):
                     command_micro = np.array([ 0, 250*fd, 0, 250*fd, 0, 250*fd, 0, 250*fd])
 
                     # check if forward is available
-                    if(data_detection[0] > 300 or data_detection[0] == 0):
+                    if(data_ultrasensor[0] > 300 or data_ultrasensor[0] == 0):
                         last_command_micro = send_command_v2(last_command_micro, command_micro, ser)
                     else:
                         # can't go forward
-                        if((data_detection[1] == 0) and (data_detection[2] == 0)):
+                        if((data_ultrasensor[1] == 0) and (data_ultrasensor[2] == 0)):
                             # if both are free, go left.
                             command_micro = np.array([ 0,    600, 0,    600, 0,    600, 0,    600])
                             last_command_micro = send_command_v2(last_command_micro, command_micro, ser)
 
-                        if(data_detection[1] > data_detection[2] and data_detection[2] != 0):
+                        if(data_ultrasensor[1] > data_ultrasensor[2] and data_ultrasensor[2] != 0):
                             # go left
                             command_micro = np.array([ 0,    600, 0,    600, 0,    600, 0,    600])
                             last_command_micro = send_command_v2(last_command_micro, command_micro, ser)
 
-                        if(data_detection[1] < data_detection[2] and data_detection[1] != 0):
+                        if(data_ultrasensor[1] < data_ultrasensor[2] and data_ultrasensor[1] != 0):
                             # go right
                             command_micro = np.array([ 0,    700, 0,    700, 0,    700, 0,    700])
                             last_command_micro = send_command_v2(last_command_micro, command_micro, ser)
 
-                        if((data_detection[1] < 300) and (data_detection[2] < 300) \
-                            and (data_detection[1] != 0) and (data_detection[2] != 0)):
+                        if((data_ultrasensor[1] < 300) and (data_ultrasensor[2] < 300) \
+                            and (data_ultrasensor[1] != 0) and (data_ultrasensor[2] != 0)):
                             # block so stop
                             new_command = False
 
