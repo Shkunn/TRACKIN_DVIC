16b66512648e3e57b42af9c0d757fb6d088e46e5
Shkunn <leclercqted@gmail.com>
2021-06-09 16:50:40 +0200

    [BUG] fixed adresse already in use

--- Trackin_DVIC/main.py
+++ Trackin_DVIC/main.py
@@ -710,7 +710,7 @@ def thread_stream_image(params):
 
     while True:
         print("Thread stream_image")
-        if new_image:
+        if new_image and copy_image_stream is not None:
             if(is_debug_option):
                 print("HZ SLAM STREAM  :", 1/(time.time() - last_time))
             last_time = time.time()
